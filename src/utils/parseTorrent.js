45ebd1cdbebda4f03c96ad06b61f74cb1fdccc2a
gineff <canone@inbox.ru>
2024-01-01 20:57:39 +0300

    fix DEP0174: calling promisify on a function that returns a Promise

--- src/utils/parseTorrent.js
+++ src/utils/parseTorrent.js
@@ -1,16 +1,16 @@
 import { remote } from 'parse-torrent'
-import { promisify } from 'node:util'
 
 export const parseTorrent = async (input) => {
-  try {
-    const promisifiedRemote = promisify(remote)
-    const torrentInfo = await promisifiedRemote(input)
-    return torrentInfo
-  } catch (error) {
+  return new Promise((resolve) => {
+    remote(input, (err, parsedTorrent) => {
+      if (err) throw err
+      resolve(parsedTorrent)
+    })
+  }).catch((error) => {
     console.error(
       'Ошибка при чтении или парсинге торрент-файла или магнет-ссылки:',
       error.message,
     )
     return null
-  }
+  })
 }
